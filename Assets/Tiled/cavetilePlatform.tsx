d76bbd040745d78f68c60bed9b77d9d49ad9fcf0
noora <s2101005@edu.bc.fi>
2024-09-09 11:58:51 +0300

    Tiled aloitettu
    Tiled kartan rakennus aloitettu

---
+++ Assets/Tiled/cavetilePlatform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cavetilePlatform" tilewidth="16" tileheight="16" tilecount="961" columns="31">
+ <image source="Cave Theme/Cave Theme/long-platforms.png" width="500" height="500"/>
+</tileset>
